89c91344966da578cd144edf8c041ec69955700c
JiSeong Lee <52674782+cocoquiet@users.noreply.github.com>
2026-08-28 18:41:19 +0900

    Implement update/delete project api (#12)
    * Chore: Change project status field name in &str
    
    * Feat: Implement update_project() method
    
    * Feat: Implement delete_project() method

--- src/integration/notion/project.rs
+++ src/integration/notion/project.rs
@@ -10,7 +10,7 @@ use super::{env::*, member::Member};
 pub enum Status {
     NotStarted,  // 시작 전
     InProgress,  // 진행 중
-    Maintenance, // 유지보유
+    Maintenance, // 유지보수
     Completed,   // 완료
 }
 
@@ -19,7 +19,7 @@ impl From<&str> for Status {
         match value {
             "시작 전" => Status::NotStarted,
             "진행 중" => Status::InProgress,
-            "유지보유" => Status::Maintenance,
+            "유지보수" => Status::Maintenance,
             "완료" => Status::Completed,
             _ => Status::NotStarted, // 기본값으로 NotStarted를 반환
         }
@@ -154,7 +154,7 @@ pub async fn get_projects() -> Result<Vec<Project>, Box<dyn std::error::Error>> 
     }
 }
 
-pub async fn create_project(project: &Project) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn create_project(project: &Project) -> Result<String, Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
 
     let response = client
@@ -172,13 +172,88 @@ pub async fn create_project(project: &Project) -> Result<(), Box<dyn std::error:
                 "status": { "select": { "name": match project.status {
                     Status::NotStarted => "시작 전",
                     Status::InProgress => "진행 중",
-                    Status::Maintenance => "유지보유",
+                    Status::Maintenance => "유지보수",
                     Status::Completed => "완료",
                 } } },
                 "github": { "url": project.github },
                 "PM": { "people": [{ "id": project.pm.id }] },
                 "participants": { "people": project.participants.iter().map(|p| serde_json::json!({ "id": p.id })).collect::<Vec<_>>() }
             }
+        }))
+        .send()
+        .await?;
+
+    if response.status().is_success() {
+        Ok(response
+            .json::<Value>()
+            .await?
+            .get("id")
+            .and_then(|id| id.as_str())
+            .unwrap_or_default()
+            .to_string())
+    } else {
+        eprintln!(
+            "Notion API 요청이 실패했습니다. Status: {}",
+            response.status()
+        );
+        Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Notion API 요청 실패",
+        )))
+    }
+}
+
+pub async fn update_project(
+    project_id: &str,
+    project: &Project,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+
+    let response = client
+        .patch(&format!("https://api.notion.com/v1/pages/{}", project_id))
+        .header("Authorization", format!("Bearer {}", get_notion_token()))
+        .header("Notion-Version", get_notion_version())
+        .header("Content-Type", "application/json")
+        .json(&serde_json::json!({
+            "properties": {
+                "name": { "title": [{ "text": { "content": project.name } }] },
+                "status": { "select": { "name": match project.status {
+                    Status::NotStarted => "시작 전",
+                    Status::InProgress => "진행 중",
+                    Status::Maintenance => "유지보수",
+                    Status::Completed => "완료",
+                } } },
+                "github": { "url": project.github },
+                "PM": { "people": [{ "id": project.pm.id }] },
+                "participants": { "people": project.participants.iter().map(|p| serde_json::json!({ "id": p.id })).collect::<Vec<_>>() }
+            }
+        }))
+        .send()
+        .await?;
+
+    if response.status().is_success() {
+        Ok(())
+    } else {
+        eprintln!(
+            "Notion API 요청이 실패했습니다. Status: {}",
+            response.status()
+        );
+        Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Notion API 요청 실패",
+        )))
+    }
+}
+
+pub async fn delete_project(project_id: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+
+    let response = client
+        .patch(&format!("https://api.notion.com/v1/pages/{}", project_id))
+        .header("Authorization", format!("Bearer {}", get_notion_token()))
+        .header("Notion-Version", get_notion_version())
+        .json(&serde_json::json!({
+            "in_trash": true
         }))
         .send()
         .await?;
